36fae07785dd1b1b3d748cad8467c7aee33c1f60
Konstantin Arakantsev <kostya3312@gmail.com>
2021-08-01 12:36:22 +0300

    fetch posts

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -5,6 +5,7 @@ import axios from "axios"
 import { stateContext } from "./StateProvider"
 
 import { Page } from "./Page"
+import { ProfilePosts } from "./ProfilePosts"
 
 export function Profile() {
     const { username } = useParams()
@@ -67,32 +68,7 @@ export function Profile() {
                 </a>
             </div>
 
-            <div className="list-group">
-                <a href="#" className="list-group-item list-group-item-action">
-                    <img
-                        className="avatar-tiny"
-                        src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-                    />{" "}
-                    <strong>Example Post #1</strong>
-                    <span className="text-muted small">on 2/10/2020 </span>
-                </a>
-                <a href="#" className="list-group-item list-group-item-action">
-                    <img
-                        className="avatar-tiny"
-                        src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-                    />{" "}
-                    <strong>Example Post #2</strong>
-                    <span className="text-muted small">on 2/10/2020 </span>
-                </a>
-                <a href="#" className="list-group-item list-group-item-action">
-                    <img
-                        className="avatar-tiny"
-                        src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-                    />{" "}
-                    <strong>Example Post #3</strong>
-                    <span className="text-muted small">on 2/10/2020 </span>
-                </a>
-            </div>
+            <ProfilePosts />
         </Page>
     )
 }
---
+++ app/components/ProfilePosts.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+
+import { Link, useParams } from "react-router-dom"
+
+export function ProfilePosts() {
+    const { username } = useParams()
+
+    const [isLoading, setIsLoading] = useState(true)
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        ;(async function fetchPosts() {
+            try {
+                const { data: posts } = await axios.get(
+                    `/profile/${username}/posts`
+                )
+
+                setPosts(posts)
+            } catch (error) {
+                console.log("There was a problem")
+            } finally {
+                setIsLoading(false)
+            }
+        })()
+    }, [])
+
+    if (isLoading) {
+        return <div>Loading ...</div>
+    }
+
+    return (
+        <div className="list-group">
+            {posts.map(({ _id: id, createdDate, avatar, title, author }) => {
+                const date = new Date(createdDate)
+
+                const dateFormatted = `${
+                    date.getMonth() + 1
+                }/${date.getDay()}/${date.getFullYear()}`
+
+                return (
+                    <Link
+                        key={id}
+                        to={`/post/${id}`}
+                        className="list-group-item list-group-item-action"
+                    >
+                        <img className="avatar-tiny" src={author.avatar} />{" "}
+                        <strong>{title}</strong>{" "}
+                        <span className="text-muted small">
+                            on {dateFormatted}
+                        </span>
+                    </Link>
+                )
+            })}
+        </div>
+    )
+}
