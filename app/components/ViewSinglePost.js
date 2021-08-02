e1867e6d7e005baede5e1572889304257a22b4fc
Konstantin Arakantsev <kostya3312@gmail.com>
2021-08-02 21:44:59 +0300

    remove post

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react"
-import { Link, useParams } from "react-router-dom"
+import React, { useCallback, useContext, useEffect, useState } from "react"
+import { Link, Redirect, useParams } from "react-router-dom"
 import ReactMarkdown from "react-markdown"
 import ReactTooltip from "react-tooltip"
 import axios from "axios"
+
+import { stateContext } from "./StateProvider"
+import { dispatchContext } from "./StateProvider"
 
 import { Page } from "./Page"
 import { NotFound } from "./NotFound"
@@ -11,7 +14,12 @@ import { Loading } from "./Loading"
 export function ViewSinglePost() {
     const { id } = useParams()
 
+    const appDispatch = useContext(dispatchContext)
+    const { user } = useContext(stateContext)
+
     const [isLoading, setIsLoading] = useState(true)
+    const [isDeletedConfirmed, setIsDeleteConfirmed] = useState(false)
+    const [isDeleteSuccessful, setIsDeleteSuccessful] = useState(false)
     const [post, setPost] = useState([])
 
     useEffect(() => {
@@ -36,6 +44,38 @@ export function ViewSinglePost() {
         }
     }, [])
 
+    useEffect(() => {
+        if (isDeletedConfirmed) {
+            const request = axios.CancelToken.source()
+
+            ;(async function deletePost() {
+                try {
+                    const response = await axios.delete(
+                        `/post/${id}`,
+                        {
+                            data: { token: user.token },
+                        },
+                        {
+                            cancelToken: request.token,
+                        }
+                    )
+
+                    if (response.data === "Success") {
+                        setIsDeleteSuccessful(true)
+                        appDispatch({
+                            type: "flashMessage",
+                            value: "Post was successfully deleted",
+                        })
+                    }
+                } catch (error) {
+                    console.log("There was a problem")
+                }
+            })()
+
+            return () => request.cancel()
+        }
+    }, [isDeletedConfirmed])
+
     if (!isLoading && !post) {
         return <NotFound />
     }
@@ -57,31 +97,53 @@ export function ViewSinglePost() {
         date.getMonth() + 1
     }/${date.getDay()}/${date.getFullYear()}`
 
+    function isOwner() {
+        return user && user.username === author.username
+    }
+
+    function deleteHandler() {
+        const areYouSure = window.confirm(
+            "Do you really wand to delete this post?"
+        )
+
+        if (areYouSure) {
+            setIsDeleteConfirmed(true)
+        }
+    }
+
+    if (isDeleteSuccessful) {
+        return <Redirect to={`/profile/${user.username}`} />
+    }
+
     return (
         <Page title={title}>
             <div className="d-flex justify-content-between">
                 <h2>{title}</h2>
 
-                <span className="pt-2">
-                    <Link
-                        data-tip="Edit"
-                        data-for="edit"
-                        to={`/post/${id}/edit`}
-                        className="text-primary mr-2"
-                    >
-                        <i className="fas fa-edit"></i>
-                    </Link>{" "}
-                    <Link
-                        data-tip="Delete"
-                        data-for="delete"
-                        to={`/post/${id}/delete`}
-                        className="delete-post-button text-danger"
-                        title="Delete"
-                    >
-                        <i className="fas fa-trash"></i>
-                    </Link>
-                </span>
+                {isOwner() && (
+                    <span className="pt-2">
+                        <Link
+                            data-tip="Edit"
+                            data-for="edit"
+                            to={`/post/${id}/edit`}
+                            className="text-primary mr-2"
+                        >
+                            <i className="fas fa-edit"></i>
+                        </Link>{" "}
+                        <Link
+                            data-tip="Delete"
+                            data-for="delete"
+                            to={`/post/${id}/delete`}
+                            className="delete-post-button text-danger"
+                            title="Delete"
+                            onClick={deleteHandler}
+                        >
+                            <i className="fas fa-trash"></i>
+                        </Link>
+                    </span>
+                )}
             </div>
+
             <p className="text-muted small mb-4">
                 <Link to={`/profile/${author.username}`}>
                     <img className="avatar-tiny" src={author.avatar} />
