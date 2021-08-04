bdbf9483e82277a042e0166bd6972a4b86d5b6ab
Konstantin Arakantsev <kostya3312@gmail.com>
2021-08-04 21:40:08 +0300

    add socket.io

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -1,12 +1,21 @@
 // todo: use hotkey to open/close chat
 
 import React, { useContext, useEffect, useRef } from "react"
+import { useImmer } from "use-immer"
+import { io } from "socket.io-client"
 
 import { stateContext, dispatchContext } from "./StateProvider"
 
+const socket = io("http://localhost:8080")
+
 export function Chat() {
-    const { isChatOpen } = useContext(stateContext)
+    const { user, isChatOpen } = useContext(stateContext)
     const dispatch = useContext(dispatchContext)
+
+    const [state, setState] = useImmer({
+        fieldValue: "",
+        chatMessages: [],
+    })
 
     const inputRef = useRef(null)
 
@@ -20,6 +29,42 @@ export function Chat() {
         }
     }, [isChatOpen])
 
+    useEffect(() => {
+        socket.on("chatFromServer", (message) => {
+            setState((state) => {
+                state.chatMessages.push(message)
+            })
+        })
+
+        return () => {}
+    }, [])
+
+    function handleFieldChange(e) {
+        setState((state) => {
+            state.fieldValue = e.target.value
+        })
+    }
+
+    function handleSubmit(e) {
+        const { fieldValue } = state
+        e.preventDefault()
+
+        // send message to chat server
+        socket.emit("chatFromBrowser", {
+            message: state.fieldValue,
+            token: user.token,
+        })
+
+        setState((state) => {
+            state.fieldValue = ""
+            state.chatMessages.push({
+                message: fieldValue,
+                username: user.username,
+                avatar: user.avatar,
+            })
+        })
+    }
+
     return (
         <div id="chat-wrapper" className={classes}>
             <div className="chat-title-bar bg-primary">
@@ -32,36 +77,49 @@ export function Chat() {
                 </span>
             </div>
             <div id="chat" className="chat-log">
-                <div className="chat-self">
-                    <div className="chat-message">
-                        <div className="chat-message-inner">
-                            Hey, how are you?
-                        </div>
-                    </div>
-                    <img
-                        className="chat-avatar avatar-tiny"
-                        src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-                    />
-                </div>
+                {state.chatMessages.map((message, index) => {
+                    if (message.username === user.username) {
+                        return (
+                            <div key={index} className="chat-self">
+                                <div className="chat-message">
+                                    <div className="chat-message-inner">
+                                        {message.message}
+                                    </div>
+                                </div>
+                                <img
+                                    className="chat-avatar avatar-tiny"
+                                    src={message.avatar}
+                                />
+                            </div>
+                        )
+                    }
 
-                <div className="chat-other">
-                    <a href="#">
-                        <img
-                            className="avatar-tiny"
-                            src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"
-                        />
-                    </a>
-                    <div className="chat-message">
-                        <div className="chat-message-inner">
+                    return (
+                        <div key={index} className="chat-other">
                             <a href="#">
-                                <strong>barksalot:</strong>
+                                <img
+                                    className="avatar-tiny"
+                                    src={message.avatar}
+                                />
                             </a>
-                            Hey, I am good, how about you?
+                            <div className="chat-message">
+                                <div className="chat-message-inner">
+                                    <a href="#">
+                                        <strong>{message.username}:</strong>
+                                    </a>{" "}
+                                    {message.message}
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    )
+                })}
             </div>
-            <form id="chatForm" className="chat-form border-top">
+
+            <form
+                onSubmit={handleSubmit}
+                id="chatForm"
+                className="chat-form border-top"
+            >
                 <input
                     ref={inputRef}
                     type="text"
@@ -69,6 +127,8 @@ export function Chat() {
                     id="chatField"
                     placeholder="Type a message…"
                     autoComplete="off"
+                    value={state.fieldValue}
+                    onChange={handleFieldChange}
                 />
             </form>
         </div>
